560d5884c7ae092e9d744babaf1fd6efe7c59a41
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-04 15:09:16 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// write your code here
+fetch("http://localhost:3000/ramens", {
+  headers: { "Content-Type": "application/json" },
+})
+  .then((res) => res.json())
+  .then((data) => renderRamen(data));
+
+function renderRamen(data) {
+  const ramenMenu = document.getElementById("ramen-menu");
+
+  data.forEach((ramen) => {
+    const ramenPic = document.createElement("img");
+    ramenPic.src = ramen.image;
+    ramenMenu.appendChild(ramenPic);
+
+    ramenPic.addEventListener("click", (e) =>
+      renderRamenDetails(data, e.target)
+    );
+  });
+}
+
+function renderRamenDetails(data, pic) {
+  const ramen = data.find((element) => {
+    console.log(element.image);
+    console.log(pic.src);
+    element.image.includes(pic.src);
+  });
+
+  console.log(ramen);
+  //   document.querySelector(".detail-image").src = ramen.image;
+}
